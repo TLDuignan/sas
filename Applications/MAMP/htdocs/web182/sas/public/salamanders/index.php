d0cdc5c3446c30bbf624ed7ad8c09591ce49f584
Taylor L Duignan <taylorlduignan@gmail.com>
2022-04-04 12:09:26 -0400

    updated public files and added db_connect and sql files

--- Applications/MAMP/htdocs/web182/sas/public/salamanders/index.php
+++ Applications/MAMP/htdocs/web182/sas/public/salamanders/index.php
@@ -1,31 +1,27 @@
 <?php require_once('../../private/initialize.php'); 
 
-$salamanders = [
-  ['id' => '1',  'salamanderName' => 'Red-Legged Salamander'],
-  ['id' => '2',  'salamanderName' => 'Pigeon Mountain Salamander'],
-  ['id' => '3',  'salamanderName' => 'ZigZag Salamander'],
-  ['id' => '4',  'salamanderName' => 'Slimy Salamander'],
-];
+  $salamander_set = find_all_salamanders();
 
-$page_title = 'Salamanders'; 
-include(SHARED_PATH . '/salamander-header.php'); 
+
+  $page_title = 'Salamanders'; 
+  include(SHARED_PATH . '/salamander-header.php'); 
 
 ?>
 
 <h1>Salamanders Main Page</h1>
 
-  <a href="<?= url_for('/salamanders/new.php'); ?>">Create a Salamander</a>
+<a href="<?= url_for('/salamanders/new.php'); ?>">Create a Salamander</a>
 
-<table>
-  <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>&nbsp;</th>
-    <th>&nbsp;</th>
-    <th>&nbsp;</th>
-  </tr>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+      </tr>
 
-      <?php foreach($salamanders as $salamander) { ?>
+      <?php while($salamander = mysqli_fetch_assoc($salamander_set)) { ?>
         <tr>
           <td><?= h($salamander['id']); ?></td>
     	    <td><?= h($salamander['salamanderName']); ?></td>
@@ -35,5 +31,9 @@ include(SHARED_PATH . '/salamander-header.php');
     	  </tr>
       <?php } ?>
   	</table>
+
+  <?php
+     mysqli_free_result($salamander_set);
+  ?>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
